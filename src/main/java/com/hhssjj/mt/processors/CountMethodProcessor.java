971939b9c246d346a83721afe049f9611c4515b4
hushengjun <1017547773@qq.com>
2017-08-09 10:55:58 +0800

    添加Count注解

---
+++ src/main/java/com/hhssjj/mt/processors/CountMethodProcessor.java
@@ -0,0 +1,13 @@
+package com.hhssjj.mt.processors;
+
+import com.hhssjj.mt.annotations.db.Count;
+
+/**
+ * Created by hushengjun on 2017/8/9.
+ */
+public class CountMethodProcessor extends BaseMethodProcessor<Count> {
+    @Override
+    public Object process() {
+        return null;
+    }
+}
