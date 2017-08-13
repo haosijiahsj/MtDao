f4f893647f018e6cbad16fe65e4a4010923f83b2
胡胜钧 <1017547773@qq.com>
2017-08-13 23:00:44 +0800

    重构

---
+++ src/main/java/com/hhssjj/mt/jdbc/JdbcOperations.java
@@ -0,0 +1,25 @@
+package com.hhssjj.mt.jdbc;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by 胡胜钧 on 8/13 0013.
+ */
+public interface JdbcOperations {
+
+    int update(String sql);
+
+    int update(PreparedStatementCreator preparedStatementCreator);
+
+    <T> T queryForObject(String sql, RowMapper<T> rowMapper);
+
+    <T> T queryForObject(String sql, PreparedStatementSetter pss, RowMapper<T> rowMapper);
+
+    Map<String, Object> queryForMap(String sql);
+
+    <T> List<T> queryForList(String sql, RowMapper<T> rowMapper);
+
+    List<Map<String, Object>> queryForList(String sql);
+
+}
