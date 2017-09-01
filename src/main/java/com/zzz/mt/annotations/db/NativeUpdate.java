51d860eff381047b2339e330afc09807025bbd22
hushengjun <1017547773@qq.com>
2017-09-01 16:18:53 +0800

    添加原生sql注解

---
+++ src/main/java/com/zzz/mt/annotations/db/NativeUpdate.java
@@ -0,0 +1,14 @@
+package com.zzz.mt.annotations.db;
+
+import java.lang.annotation.*;
+
+/**
+ * Created by hushengjun on 2017/9/1.
+ */
+@Documented
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface NativeUpdate {
+    String value();
+    boolean returnId() default false;
+}
